d4e51a2d86da48d0bc7170551207f8461be5491e
Sohail M. Khan <sokhan@mediamath.com>
2022-09-19 23:17:08 +0800

    add react + partytown (dataLayer) + netlify (edge)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
+
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), partytown({
+    config: { forward: ['dataLayer.push'] },
+  })],
+  output: "server",
+  adapter: netlify()
+});
